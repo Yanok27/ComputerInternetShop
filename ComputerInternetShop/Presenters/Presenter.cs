c6291cb635fb588054b74e92ebc1d35ece0595dd
SmileRedemption <ismaylov.den@gmail.com>
2023-05-21 22:54:55 +0300

    change presenter

--- ComputerInternetShop/Presenters/Presenter.cs
+++ ComputerInternetShop/Presenters/Presenter.cs
@@ -10,74 +10,90 @@ namespace ComputerInternetShop.Presenters
         private readonly IMenuView _menuView;
         private readonly IProductSearcher _productSearcher;
 
+        private readonly Dictionary<ActionType, Func<IEnumerable<Product>>> _actionMethods;
+
+        private void OnActionChoice(ActionType actionType)
+        {
+            if (actionType == ActionType.None)
+            {
+                Console.WriteLine("Невірний тип!");
+                Disable();
+                return;
+            }
+
+            if (_actionMethods.TryGetValue(actionType, out var actionMethod))
+                _menuView.ShowResult(actionMethod.Invoke());
+            else
+                throw new ArgumentOutOfRangeException(nameof(actionType), actionType, null);
+
+            Disable();
+        }
+        
         public Presenter(IMenuView menuView, IProductSearcher productSearcher)
         {
             _menuView = menuView;
             _productSearcher = productSearcher;
+
+            _actionMethods = new Dictionary<ActionType, Func<IEnumerable<Product>>>()
+            {
+                {
+                    ActionType.FindByActionNumber,
+                    () => _productSearcher.FindProductByItemNumber(_menuView.GetItemNumber())
+                },
+                {
+                    ActionType.FindByName, 
+                    () => _productSearcher.FindProductByName(_menuView.GetName())
+                },
+                {ActionType.FindByPrice, () => _productSearcher.FindProductByPrice(_menuView.GetPrice())},
+                {
+                    ActionType.FindBySocketType,
+                    () => _productSearcher.FindProductBySocketType(_menuView.GetSocketType())
+                },
+                {
+                    ActionType.FindProductByChipset, 
+                    () => _productSearcher.FindProductByChipset(_menuView.GetChipset())
+                },
+                {
+                    ActionType.FindProductByMemoryType,
+                    () => _productSearcher.FindProductByMemoryType(_menuView.GetMemoryType())
+                },
+                {
+                    ActionType.FindProductByBusSpeed,
+                    () => _productSearcher.FindProductByBusSpeed(_menuView.GetBusSpeed())
+                },
+                {
+                    ActionType.FindProductByCoreCount,
+                    () => _productSearcher.FindProductByCoreCount(_menuView.GetCoreCount())
+                },
+                {
+                    ActionType.FindProductByClockSpeed,
+                    () => _productSearcher.FindProductByClockSpeed(_menuView.GetClockSpeed())
+                },
+                {
+                    ActionType.FindProductByCapacity,
+                    () => _productSearcher.FindProductByCapacity(_menuView.GetCapacity())
+                },
+                {
+                    ActionType.FindProductByFrequency,
+                    () => _productSearcher.FindProductByFrequency(_menuView.GetFrequency())
+                },
+                {
+                    ActionType.FindProductByModuleCount,
+                    () => _productSearcher.FindProductByModuleCount(_menuView.GetModuleCount())
+                },
+                {
+                    ActionType.FindProductBySpeed, 
+                    () => _productSearcher.FindProductBySpeed(_menuView.GetBusSpeed())
+                },
+                {
+                    ActionType.FindProductByInterfaceOfConnect,
+                    () => _productSearcher.FindProductByInterfaceOfConnect(_menuView.GetInterfaceOfConnect())
+                }
+            };
         }
-        
+
         public void Enable() => _menuView.ActionChoice += OnActionChoice;
 
         public void Disable() => _menuView.ActionChoice -= OnActionChoice;
-
-        private void OnActionChoice(ActionType actionType)
-        {
-            IEnumerable<Product> result;
-            
-            switch (actionType)
-            {
-                case ActionType.None:
-                    Console.WriteLine("Невірний тип!");
-                    Disable();
-                    return;
-                case ActionType.FindByActionNumber:
-                    result = _productSearcher.FindProductByItemNumber(_menuView.GetItemNumber());
-                    break;
-                case ActionType.FindByName:
-                    result = _productSearcher.FindProductByName(_menuView.GetName());
-                    break;
-                case ActionType.FindByPrice:
-                    result = _productSearcher.FindProductByPrice(_menuView.GetPrice());
-                    break;
-                case ActionType.FindBySocketType:
-                    result = _productSearcher.FindProductBySocketType(_menuView.GetSocketType());
-                    break;
-                case ActionType.FindProductByChipset:
-                    result = _productSearcher.FindProductByChipset(_menuView.GetChipset());
-                    break;
-                case ActionType.FindProductByMemoryType:
-                    result = _productSearcher.FindProductByMemoryType(_menuView.GetMemoryType());
-                    break;
-                case ActionType.FindProductByBusSpeed:
-                    result = _productSearcher.FindProductByBusSpeed(_menuView.GetBusSpeed());
-                    break;
-                case ActionType.FindProductByCoreCount:
-                    result = _productSearcher.FindProductByCoreCount(_menuView.GetCoreCount());
-                    break;
-                case ActionType.FindProductByClockSpeed:
-                    result = _productSearcher.FindProductByClockSpeed(_menuView.GetClockSpeed());
-                    break;
-                case ActionType.FindProductByCapacity:
-                    result = _productSearcher.FindProductByCapacity(_menuView.GetCapacity());
-                    break;
-                case ActionType.FindProductByFrequency:
-                    result = _productSearcher.FindProductByFrequency(_menuView.GetFrequency());
-                    break;
-                case ActionType.FindProductByModuleCount:
-                    result = _productSearcher.FindProductByModuleCount(_menuView.GetModuleCount());
-                    break;
-                case ActionType.FindProductBySpeed:
-                    result = _productSearcher.FindProductBySpeed(_menuView.GetBusSpeed());
-                    break;
-                case ActionType.FindProductByInterfaceOfConnect:
-                    result = _productSearcher.FindProductByInterfaceOfConnect(_menuView.GetInterfaceOfConnect());
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(actionType), actionType, null);
-            }
-            
-            _menuView.ShowResult(result);
-            Disable();
-        }
     }
 }
